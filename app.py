9a9da90b82525f84c63756142cd84ede6a1a5d70
Всеволод <kreepmeister@gmail.com>
2023-06-27 11:33:47 +0300

    Fixes

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["API_SCHEMES"] = ["https"]
 CORS(app)
 
 
-@app.route('/')
+@app.route("/")
 async def test(request):
     """
     Test route
@@ -31,13 +31,13 @@ async def test(request):
         description: How many items to return at one time (max 100)
         required: true
     """
-    return json({'hello': 'world'})
+    return json({"hello": "world"})
 
 
 @app.exception(Exception)
 async def handle_exception(request, exception):
-    return json({'error': str(exception)}, status=400)
-    
+    return json({"error": str(exception)}, status=400)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True, auto_reload=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True, auto_reload=True)
